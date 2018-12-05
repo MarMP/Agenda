a8e72fd2c00a4ac1bece2be10dc32ce6f39ed4b3
Mar <marmembriveplaza@gmail.com>
2018-12-05 21:36:31 +0100

    Commit atributos de clase agenda, get y método getContactos

--- src/main/java/org/iesalandalus/programacion/agenda/Agenda.java
+++ src/main/java/org/iesalandalus/programacion/agenda/Agenda.java
@@ -3,7 +3,21 @@ package org.iesalandalus.programacion.agenda;
 public class Agenda {
 
 	private static int MAX_CONTACTOS = 3;
-	private int numContactos;
+	private int numContactos = 0;
+	private Contacto [] contactos; 
+	
+	public Agenda() {
+	}
+
+	public int getNumContactos() {
+		return numContactos;
+	}
+	
+	public Contacto [] getContactos() {
+		Contacto [] copiaContactos = contactos;
+		return copiaContactos;
+	}
+	
 	
 	
 }
